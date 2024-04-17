e5c4956488edef46e6d26027dd9f00423b5d482c
githubscientist <sathish.datascientist@gmail.com>
2024-04-17 13:14:02 +0530

    backend completed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 
 // enable all CORS requests
 app.use(cors({
-    origin: '*', // allow all origins
+    origin: 'http://localhost:5173', // allow all origins
     credentials: true
 }));
 
